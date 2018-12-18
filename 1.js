3f7dfe03cbd81bea4e848ce2757bb54f679142bc
toan6697 <ductoan661997@gmail.com>
2018-12-19 02:51:22 +0700

    hoan thien phan footer

--- 1.js
+++ 1.js
@@ -1,4 +1,9 @@
 ﻿ $(function(){
- 
+     $('.chuot,.kn').click(function(event) {
+     	$('html,body').animate({ scrollTop: $(".edu").offset().top}, 500)
+     });
+     $('.sp').click(function(event) {
+     	$('html,body').animate({ scrollTop: $(".cacsp").offset().top}, 500)
+     });
 })  
  
