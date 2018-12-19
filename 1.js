efd34ea156bd571c5598f7b7be4cdd2afaee6990
toan6697 <ductoan661997@gmail.com>
2018-12-19 21:47:39 +0700

    ok roi

--- 1.js
+++ 1.js
@@ -6,8 +6,10 @@
      	$('html,body').animate({ scrollTop: $(".cacsp").offset().top}, 500)
      });
      $('.nutlen').click(function(event) {
-     	console.log('abc');
      	$('html,body').animate({ scrollTop:0}, 500)
+     });
+     $('.cmt').click(function(event) {
+     	$('html,body').animate({ scrollTop:$(".facebookcomment").offset().top}, 500)
      });
 })  
  
